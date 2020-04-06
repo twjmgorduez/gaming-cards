1a8ada79dbecb631506b1cb408a2b915718f6da0
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-06 13:39:12 -0500

    Configure dev env

--- tests/appTest.js
+++ tests/appTest.js
@@ -3,8 +3,10 @@ import { add } from '../src/index'
 
 describe('Adding 2 numbers', () => { 
     it('should return 3 given 1 and 2', () => {
-        const sum = add(1, 2)
+        
         const sumExpected = 3
+
+        const sum = add(1, 2)
 
         expect(sum).to.equals(sumExpected)
     })
