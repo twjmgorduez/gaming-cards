81560759d2fbcc7ebd522fb514cd0b7a19bb75fd
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-08 09:34:49 -0500

    Testing to develop a test for an API

--- tests/product_gallery_test.js
+++ tests/product_gallery_test.js
@@ -1,18 +1,17 @@
-import expect  from 'chai'
 import request from 'supertest'
 import app from '../src/index'
 
 describe('Product gallery', () => { 
     it('should return all of products', (done) => {
         
-        const products_expected = "['Card 1','Card 2','Card 3']"
+        const products_expected = '["Card 1","Card 2","Card 3"]'
 
         request(app)
             .get('/show-gallery')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(200, done)
-            //.expect(products_expected, done)
+            .expect(200)
+            .expect(products_expected, done)
     })
 
     it('should return Card 1 given Card 1', (done) => {
@@ -23,7 +22,7 @@ describe('Product gallery', () => {
             .get('/show-product/Card 1')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(200, done)
-            //.expect(products_expected, done)
+            .expect(200)
+            .expect(JSON.stringify(product_expected), done)
     })
 })
