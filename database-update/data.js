2681cc388b06122d3b5e6e71ea51eb7ebc7845fd
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-12 16:35:16 -0500

    Testing to run database update scripts

--- database-update/data.js
+++ database-update/data.js
@@ -1,6 +1,6 @@
 
 const arr = [{_id: 1,name: "Card 1"},{_id: 2,name: "Card 2"},{_id: 3,name: "Card 3"},{_id: 4,name: "Card 4"}];
-insertMany(arr);
+db.collection.insertMany(arr);
 
 cursor = db.collection.find();
 while ( cursor.hasNext() ) {
