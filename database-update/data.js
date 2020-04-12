823d6781a09edc850f70d583693849eb92303404
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-12 16:49:25 -0500

    Testing to run database update scripts

--- database-update/data.js
+++ database-update/data.js
@@ -1,8 +1,8 @@
 
-const arr = [{_id: 1,name: "Card 1"},{_id: 2,name: "Card 2"},{_id: 3,name: "Card 3"},{_id: 4,name: "Card 4"}];
-db.collection.insertMany(arr);
+const arr = [{_id: 1,name: "Card 1",_v: 0},{_id: 2,name: "Card 2",_v: 0},{_id: 3,name: "Card 3",_v: 0},{_id: 4,name: "Card 4",_v: 0}];
+"product-gallery".card.insertMany(arr);
 
-cursor = db.collection.find();
+const cursor = "product-gallery".card.find();
 while ( cursor.hasNext() ) {
    printjson( cursor.next() );
 }
--- database-update/schema.js
+++ database-update/schema.js
@@ -1,5 +1,13 @@
 
-cursor = db.collection.find();
+"product-gallery".createCollection( "card",
+    {
+      _id: number,
+      name: string,
+      _v: number
+    }
+ )
+
+const cursor = "product-gallery".find();
 while ( cursor.hasNext() ) {
    printjson( cursor.next() );
 }
