b694de5c2ef50631dd3becf05204daecc9c30869
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-13 08:40:19 -0500

    Testing to deploy full app

--- database-update/data.js
+++ database-update/data.js
@@ -1,6 +1,6 @@
 
-//const arr = [{_id: 1,name: "Card 1",_v: 0},{_id: 2,name: "Card 2",_v: 0},{_id: 3,name: "Card 3",_v: 0},{_id: 4,name: "Card 4",_v: 0}];
-//db.card.insertMany(arr);
+const arr = [{_id: 1,name: "Card 1",_v: 0},{_id: 2,name: "Card 2",_v: 0},{_id: 3,name: "Card 3",_v: 0},{_id: 4,name: "Card 4",_v: 0}];
+db.card.insertMany(arr);
 
 const cursor = db.card.find();
 while ( cursor.hasNext() ) {
